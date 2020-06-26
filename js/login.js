88a12016b798a347dd30251d60a9ac64db6b16d8
1338199 <1005089526@qq.com>
2020-06-26 10:40:19 +0800

    .

---
+++ js/login.js
@@ -0,0 +1,41 @@
+window.onload = function () {
+    model.init(function () {
+        var data = model.data;
+        var btn = $('.btn');
+        if(btn!==null) {
+            btn.addEventListener('click', function (ev) {
+                var username = $('#username').value;
+                var pass1 = $('#pass1').value;
+                var valid = false;
+                var wrongpass = false;
+                var userID = 1;
+                if (username === '' || pass1 === '' ) {
+                    alert("Username or password can't be empty");
+                    return;
+                }
+                data.users.forEach(function (user) {
+                    if(user.name === username){
+                        userID = user.userId;
+                        valid = true;
+                        if(user.pass !== pass1){
+                            wrongpass = true;
+                        }
+
+                    }
+                });
+                if(!valid){
+                    alert("Invalid User Name");
+                    return;
+                }
+                if(wrongpass){
+                    alert("wrong password");
+                    return;
+                }
+                window.localStorage.username = username;
+                window.localStorage.userId = userID;
+                window.location.href = "index.html";
+            })
+        }
+    });
+
+};
---
+++ js/model.js
@@ -0,0 +1,14 @@
+window.model = {
+    data: {
+        users:[
+
+        ],
+        items: [
+            // {msg:'jxh', completed: false, dateStr: '2020.6.21',userId:'0'},
+            // {msg:'hp', completed: false,  dateStr: '2020.6.21',userId:'0'},
+        ],
+        msg: '',
+        filter: 'All',
+    },
+    TOKEN: 'TodoMVC'
+};
